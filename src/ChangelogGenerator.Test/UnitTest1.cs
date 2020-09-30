832bd166337e66a2d868a0915935ba57dbccea66
Darius Weber <darius.weber@posteo.de>
2020-09-30 19:49:05 +0200

    Initialize new project

---
+++ src/ChangelogGenerator.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+
+namespace ChangelogGenerator.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
