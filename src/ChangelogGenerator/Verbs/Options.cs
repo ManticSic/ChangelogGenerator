cd8e5d7d6ceafbbf73c0d40eeb7c5619b79f5a37
Darius Weber <darius.weber@posteo.de>
2020-10-07 19:25:15 +0200

    Add default cli options

---
+++ src/ChangelogGenerator/Verbs/Options.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+
+
+namespace ChangelogGenerator.Verbs
+{
+    internal class Options
+    {
+        [Option("product",
+                Required = true,
+                HelpText =
+                    "The name (and optionally version) of the product using this library, the name of your GitHub organization, or your GitHub username (in that order of preference).")]
+        public string ProductInformation { get; set; }
+
+        [Option("repository",
+                Required = false,
+                Default  = null,
+                HelpText = "Set the repository name.")]
+        public string Repository { get; set; }
+
+        [Option("owner",
+                Required = false,
+                Default  = null,
+                HelpText = "Set the owner of the repository.")]
+        public string Owner { get; set; }
+
+        [Option("token",
+                Required = false,
+                Default = null,
+                HelpText = "GitHub authenfication token.")]
+        public string Token { get; set; }
+
+        [Option('o', "output",
+                Required = false,
+                Default  = "CHANGELOG.md",
+                HelpText = "Set output file name. Default: CHANGELOG.md.")]
+        public string Output { get; set; }
+
+        [Option('v', "verbose",
+                Required = false,
+                Default = false,
+                HelpText = "Be verbose.")]
+        public bool Verbose { get; set; }
+    }
+}
