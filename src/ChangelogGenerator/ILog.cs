821e98921bb26fc9f38fd0730b911f81eec9c543
Darius Weber <darius.weber@posteo.de>
2020-10-07 19:27:49 +0200

    Add logger

---
+++ src/ChangelogGenerator/ILog.cs
@@ -0,0 +1,21 @@
+﻿namespace ChangelogGenerator
+{
+    internal interface ILog
+    {
+        public void Info(object value);
+
+        public void Info(string value);
+
+        public void VerboseInfo(object value);
+
+        public void VerboseInfo(string value);
+
+        public void Error(object value);
+
+        public void Error(string value);
+
+        public void VerboseError(object value);
+
+        public void VerboseError(string value);
+    }
+}
