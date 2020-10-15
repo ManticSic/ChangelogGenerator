979becb2d10225eab39ef72b3dcb69f28db22ea3
Darius Weber <darius.weber@posteo.de>
2020-10-15 22:46:13 +0200

    Add tests for UnknownVerbHandler

---
+++ src/ChangelogGenerator.Test/Verbs/UnknownVerbHandlerTest.cs
@@ -0,0 +1,20 @@
+﻿using ChangelogGenerator.Verbs;
+
+using NUnit.Framework;
+
+
+namespace ChangelogGenerator.Test.Verbs
+{
+    public class UnknownVerbHandlerTest
+    {
+        [Test]
+        public void TestRun()
+        {
+            IVerbHandler verbHandler = new UnknownVerbHandler();
+
+            verbHandler.Run();
+
+            Assert.Pass();
+        }
+    }
+}
