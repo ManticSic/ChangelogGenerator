8d7d9ad6b32183eaef634752e41888eb2c997345
Darius Weber <darius.weber@posteo.de>
2020-10-18 21:24:02 +0200

    Code cleanup

--- src/ChangelogGenerator/Generator.cs
+++ src/ChangelogGenerator/Generator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
 using System.Reflection;
-using System.Xml.Serialization;
 
 using ChangelogGenerator.Verbs;
 using ChangelogGenerator.Verbs.Generate;
--- src/ChangelogGenerator/Verbs/Generate/GenerateHandler.cs
+++ src/ChangelogGenerator/Verbs/Generate/GenerateHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Threading.Tasks;
 
